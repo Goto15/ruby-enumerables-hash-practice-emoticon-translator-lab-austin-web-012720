9cfe6c6e4793bcd00b3e098ffa54d0de43a7ed52
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-21 05:06:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'pp'
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
-  emoticons["get_meaning"] = {}
+  emoticons["get_meaning"] = ""
 end
 
 def get_japanese_emoticon
