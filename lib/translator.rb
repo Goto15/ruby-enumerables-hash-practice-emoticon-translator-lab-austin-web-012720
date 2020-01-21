913e6edc6d7ef1f49df24725031ff44ba8dbb76f
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-21 05:20:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,8 +8,8 @@ def load_library(file_path)
   emoticons = {}
   emoticon_meaning = data.keys
   for each in emoticon_meaning
-    pp each[1]
-    emoticons[:get_meaning] = { each => data[each][1][1] }
+    pp data[each][1]
+    emoticons[:get_meaning] = { each => data[each][1] }
     emoticons[:get_emoticon] = { each => each[0] }
   end
   
